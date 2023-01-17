df9e60ec3c1fd57555100131970c3cab335a326d
khalid fifel <fifle.khalid99@gmail.com>
2023-01-17 14:46:56 +0100

    add category page of admin

---
+++ admin/category.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+
+<?php
+$title = "category";
+require_once "includes/header.php";
+?>
+
+<body class="flex">
+
+<?php
+$page = "category";
+include "./includes/sidebar.php";
+?>
+
+<section class="main">
+    <?php include "./includes/navbar.php";?>
+
+
+
+    <main class="main-content bg-yellow-600">
+
+        <h1>category</h1>
+
+    </main>
+
+
+
+    <?php include "./includes/footer.php";?>
+</section>
+
+<?php
+include "./includes/scripts-js.php";
+?>
+
+</body>
+</html>
