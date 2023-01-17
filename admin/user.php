9340b90fc2e7912f3b50581de3170a388da3484b
khalid fifel <fifle.khalid99@gmail.com>
2023-01-17 14:47:36 +0100

    add manager user page

---
+++ admin/user.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+
+<?php
+$title = "user";
+require_once "includes/header.php";
+?>
+
+<body class="flex">
+
+<?php
+$page = "user";
+include "./includes/sidebar.php";
+?>
+
+<section class="main">
+    <?php include "./includes/navbar.php";?>
+
+
+
+    <main class="main-content bg-yellow-600">
+
+        <h1>user</h1>
+
+    </main>
+
+
+
+    <?php include "./includes/footer.php";?>
+</section>
+
+<?php
+include "./includes/scripts-js.php";
+?>
+
+</body>
+</html>
