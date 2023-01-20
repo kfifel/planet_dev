2bbc0ed96808629185e5e10091061da640f4383c
khalid fifel <fifle.khalid99@gmail.com>
2023-01-20 23:29:56 +0100

    update the getMapping function in js

--- assets/scripts/js/ajax.js
+++ assets/scripts/js/ajax.js
@@ -1,15 +1,6 @@
 
  async function getMapping(URL){
-     let ajax = new XMLHttpRequest();
-     ajax.open('GET', URL, true);
-     ajax.onreadystatechange = ()=>{
-         if(ajax.status === 200 && ajax.readyState === 4 ) {
-            return ajax.response;
-         }
-     }
-     ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded"); //format submitting form
-     ajax.send();
-
+     return await fetch(URL);
  }
 
  async function postMapping(URL, data) {
