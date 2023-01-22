bfb4c7fda5a1cda298f006e0b95316620c07ab16
khalid fifel <fifle.khalid99@gmail.com>
2023-01-22 17:44:10 +0100

    add the update function back-end

--- src/includes/router.php
+++ src/includes/router.php
@@ -10,6 +10,9 @@ if(!session_id()){
 if(isset($_GET['createArticles']))  // "php://input" is a read-only stream that allows you to read raw data from the request body
     $_SESSION['admin']->adminController->createArticles( json_decode(file_get_contents('php://input'), true));
 
+if(isset($_GET['updateArticle']))  // "php://input" is a read-only stream that allows you to read raw data from the request body
+    echo $_SESSION['admin']->updateArticle( json_decode(file_get_contents('php://input'), true)) ? "true" : "false";
+
 if(isset($_GET['deleteArticles']))
     $_SESSION['admin']->deleteArticle($_GET['id']);
 
@@ -18,3 +21,8 @@ if(isset($_GET['getAllArticles']))
 
 if(isset($_GET['searchArticle']))
     echo json_encode($_SESSION['admin']->searchArticle($_GET['searchArticle']));
+
+
+if(isset($_GET['getArticleById']))
+    echo json_encode($_SESSION['admin']->getArticleById($_GET['id']));
+
--- src/model/Admin.php
+++ src/model/Admin.php
@@ -50,27 +50,70 @@ class Admin extends Person
     }
 
 
-    public function getArticleById( $id): Article|null
+    public function getArticleById( $id): array
     {
-
-        return null;
-    }
-
-    public function updateArticle( $id, Article $article): bool
-    {
-
-        return false;
+        return Database::connect()->query("select * from article where id=$id ")->fetch(PDO::FETCH_ASSOC);
     }
 
 
-    public function getAllArticle(): array
+    public function searchArticle( $search , $order='id'): array
     {
         return Database::connect()->query("
-                select a.id as id, a.title as title, a.content as content, a.published_date as published_date, c.name as category, concat(a2.last_name , ' ', a2.first_name) as username
+                select a.id as id, a.title as title, a.content as content, a.published_date as published_date,
+                       c.name as category, concat(a2.last_name , ' ', a2.first_name) as author, a2.id as id_author, c.id as id_category
                 from article a 
                     inner join author a2 on a.author_id = a2.id
                     inner join category c on a.category_id = c.id  
+                    where  
+                          a.title  like '%".$search."%' or  a.content  like '%".$search."%' 
+                order by $order
                     ")
+            ->fetchAll(PDO::FETCH_ASSOC)
+            ;
+    }
+
+    public function updateArticle( array $article): bool
+    {
+        if( !empty($article['title']) && !empty($article['content']) && !empty($article['author'])  && !empty($article['category'])
+            && is_numeric($article['author']) && is_numeric($article['category']) && is_numeric($article['id'])
+        ):
+            $query = "update article
+                    set
+                title = :title , content =:content, published_date=:published_date, category_id=:category_id, author_id=:author_id
+                where id= :id
+                ";
+            $sth = Database::connect()->prepare($query);
+
+            $sth->bindValue(':id', $article['id']);
+            $sth->bindValue(':title', $article['title']);
+            $sth->bindValue(':content', $article['content']);
+            $sth->bindValue(':published_date', date("Y-m-d") );
+            $sth->bindValue(':author_id', $article['author'] );
+            $sth->bindValue(':category_id', $article['category']);
+            return $sth->execute();
+        else:
+            return false;
+        endif;
+    }
+
+
+    public function getAllArticle( $order='id'): array
+    {
+        return Database::connect()->query("
+                select a.id as id, a.title as title, a.content as content, a.published_date as published_date, c.name as category,
+                       concat(a2.last_name , ' ', a2.first_name) as author, a2.id as id_author, c.id as id_category
+                from article a 
+                    inner join author a2 on a.author_id = a2.id
+                    inner join category c on a.category_id = c.id 
+                order by $order
+                    ")
+            ->fetchAll(PDO::FETCH_ASSOC)
+        ;
+    }
+
+    public function getAllCategories(): array
+    {
+        return Database::connect()->query("select * from category ")
             ->fetchAll(PDO::FETCH_ASSOC)
         ;
     }
