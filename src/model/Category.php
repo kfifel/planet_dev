46e1a4e463f5aface049c88ecaaeb121a66fd2bf
khalid fifel <fifle.khalid99@gmail.com>
2023-01-24 00:05:04 +0100

    add getters and setters to Category model

--- src/model/Category.php
+++ src/model/Category.php
@@ -13,4 +13,37 @@ class Category
         $this->id = $id;
         $this->name = $name;
     }
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param int|null $id
+     */
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
 }
