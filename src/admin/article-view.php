2ae988fe0483bc312a89d84210b51f041da63577
khalid fifel <fifle.khalid99@gmail.com>
2023-01-23 11:31:27 +0100

    redirecting to the article overview when user click in article breadcrumb in article-view

--- src/admin/article-view.php
+++ src/admin/article-view.php
@@ -27,22 +27,22 @@ include "./includes/sidebar.php";
                         Home
                     </a>
                 </li>
-                <li aria-current="page">
-                    <a href="article.php" class="flex items-center">
+                <li class="inline-flex items-center">
+                    <a href="article.php" class="inline-flex items-center ">
                         <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-                        <span class="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">Article</span>
+                        <span class="ml-1 text-sm font-medium text-gray-500 md:ml-2 hover:text-blue-600">Article</span>
                     </a>
                 </li>
                 <li aria-current="page">
                     <div class="flex items-center">
                         <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-                        <span class="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">Overview</span>
+                        <span class="ml-1 text-sm font-medium text-gray-500 md:ml-2 ">View</span>
                     </div>
                 </li>
             </ul>
         </div>
 
-        <h1 class="text-2xl text-bold my-6">View article</h1>
+        <h1 class="text-2xl font-bold my-6">View article</h1>
 
         <?php
             $article =$_SESSION['admin']->getArticleById($_GET['id']);
