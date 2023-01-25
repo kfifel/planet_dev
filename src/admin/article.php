5da8fee31685cd76895049483000a17b4ed33e87
khalid fifel <fifle.khalid99@gmail.com>
2023-01-25 09:37:39 +0100

    fix the bug of the model showing up in the breakpoint md

--- src/admin/article.php
+++ src/admin/article.php
@@ -75,7 +75,7 @@
                 </div>
             </div>
             <div class="overflow-x-auto">
-                <table class="table table-zebra w-full drop-shadow-xl " id="tableArticles">
+                <table class="table table-zebra w-full drop-shadow-xl md:table-fixed sm:table-auto " id="tableArticles">
                     <thead>
                     <tr>
                         <th>title</th>
@@ -125,8 +125,7 @@
 
 
 
-
-        <div id="article-modal" tabindex="-1" aria-hidden="true" class="fixed top-[50%] left-[50%] right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
+        <div id="article-modal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
             <div class="relative w-full h-full max-w-md md:h-auto">
                 <!-- Modal content -->
                 <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
