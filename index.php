5aec5124c166e06c0564753596aa5fdf6cf11a68
khalid fifel <fifle.khalid99@gmail.com>
2023-01-26 00:18:10 +0100

    styling login page

--- index.php
+++ index.php
@@ -5,26 +5,35 @@
     <meta name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <title>Log in</title>
 </head>
-<body>
-    <div class="container">
-
-        <form action="src/controller/auth.php" method="post">
-            <div class="form-control">
-                <label for="email">email :</label>
-                <input type="text" name="email" id="email" placeholder="">
+<body class="login">
+    <section class="bg-gray-50">
+        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <h1 class="font-bold text-gray-900 md:text-4xl flex justify-center">
+                        <i class="fas fa-lock"></i>
+                    </h1>
+                    <form class="space-y-4 md:space-y-6" action="src/controller/auth.php" method="post">
+                        <div>
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lgblock w-full p-2.5" placeholder="name@company.com" required="">
+                        </div>
+                        <div>
+                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                            <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5" required="">
+                        </div>
+                        <button type="submit" name="login" class="w-full text-white bg-blue-600 hover:bg-blue-700 font-medium rounded-lg text-sm my-7 px-5 py-2.5 text-center">Sign in</button>
+                </div>
             </div>
+        </div>
+    </section>
 
-            <div class="form-control">
-                <label for="password">password :</label>
-                <input type="password" name="password" id="password" placeholder="">
-            </div>
-
-            <div>
-                <button class="btn btn-primary" name="login" id="connexion">Connexion</button>
-            </div>
-        </form>
-    </div>
+    <script src="assets/scripts/tailwind/main.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.1/flowbite.min.js"></script>
+    <script src="assets/scripts/js/ajax.js"></script>
+    <script src="assets/scripts/js/main.js"></script>
 </body>
 </html>
